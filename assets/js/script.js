99f04b3004f849f764300a004f61e5e6295d5565
Lex <info@alexandranel.com>
2024-01-18 21:03:35 +1100

    updated some javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-var domainAPIKey = "key_a00792ad1bdc28da797b26f36a544d57";
+var APIKey = "3d535884f42f455f9f5e3299842beecb";
+var textInput=;
+var cityInput =;
+var countryInput =;
 
-function getDomainAPI(){
+
+Parameters:
+// [number]
+// [text]
+// [sort-direction] ASC or DESC
+// [location-filter] - latitude, longitude, radius
+
+// error code 402 for call limit
+// error code 429 for exceeding 60 requests
+
+function getnewsAPI(){
+
+    //function to catch spelling errors goes here
+    // if() {
+    // }else{
+    //     window.alert("please enter a valid city")
+    // }
+
+    var queryURL = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=${text}&location-filter${lat, lon, rad}`;
+
+
 
 
 }
