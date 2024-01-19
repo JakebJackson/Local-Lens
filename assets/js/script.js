e8257d6a366fc700a48cd723c90f9a522724b29c
Lex <info@alexandranel.com>
2024-01-19 21:52:33 +1100

    have written the api call and a function to construct the url with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 var APIKey = "3d535884f42f455f9f5e3299842beecb";
-var textInput=;
-var cityInput =;
-var countryInput =;
+var keywordInput= document.getElementById("keyword");
+var radiusInput= document.getElementById("radius");
 
 
-Parameters:
+// Parameters:
 // [number]
 // [text]
 // [sort-direction] ASC or DESC
@@ -13,17 +12,44 @@ Parameters:
 // error code 402 for call limit
 // error code 429 for exceeding 60 requests
 
-function getnewsAPI(){
+//getting the api string from the user input
+function createCallIUrl(lat, lon) {
 
+    var queryURL = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}`;
+
+    if (keywordInput.value) {
+        queryURL += `&text=${keywordInput}`;
+    }
+
+    if (radiusInput.value) {
+        queryURL += `&location-filter=${lat, lon, radiusInput}`
+    } else {
+        queryURL += `&location-filter=${lat, lon}`
+    };
+}
+
+function getDataApi(queryURL) {
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    latestNews(data) 
+                });
+            } else {
+                alert("Error"+ response.statusText);
+            }
+        })
+        .catch(function (error){
+            alert("Unable to connect to headlines, try again later")
+        });
+    };
+
+// var queryURL = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=${keywordInput}&location-filter${lat, lon, radiusInput}`;
     //function to catch spelling errors goes here
     // if() {
     // }else{
     //     window.alert("please enter a valid city")
     // }
-
-    var queryURL = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=${text}&location-filter${lat, lon, rad}`;
-
-
-
-
-}
